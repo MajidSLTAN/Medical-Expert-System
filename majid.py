1d3da0665d25ba17ae56f65a6f5d9d17ea79a4c0
MAJID <160159330+MajidSLTAN@users.noreply.github.com>
2025-01-30 22:44:55 +0300

    Update majid.py

--- majid.py
+++ majid.py
@@ -1,5 +1,5 @@
 import os
-os.system('pip install arabic_reshaper bidi networkx matplotlib pillow speechrecognition streamlit')
+os.system('pip install arabic_reshaper bidi networkx matplotlib pillow speechrecognition streamlit json PIL')
 import streamlit as st
 import json
 import arabic_reshaper
